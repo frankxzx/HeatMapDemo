368dc5d567fd631e31f2fbbfeaf5face545c40a8
frank.xu <frank.xu@touch-spring.com>
2016-05-10 17:32:39 +0800

    init

--- HeatMapDemo/ViewController.swift
+++ HeatMapDemo/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let bundle = NSBundle.mainBundle()
+        let urlStr = bundle.pathForResource("echarts", ofType: "html")
+        let url = NSURL(string: urlStr!)
+        let req = NSURLRequest(URL: url!)
+        webView.loadRequest(req)
     }
-
+    
+    @IBAction func reloadCharts(sender: AnyObject) {
+        webView.reload()
+    }
 
 }
 
+extension ViewController: UIWebViewDelegate {
+
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        return true
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+    
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+    
+    }
+    
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+    
+    }
+
+}
